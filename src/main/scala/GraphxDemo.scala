8eb92b7b12ec4a1c69129c1822f060325c7181df
xxh13 <15850781776@163.com>
2017-11-08 21:50:33 +0800

    ....

--- src/main/scala/GraphxDemo.scala
+++ src/main/scala/GraphxDemo.scala
@@ -10,7 +10,7 @@ object GraphxDemo{
 
   def main(args: Array[String]): Unit = {
     val mongoClient: MongoClient = new MongoData().getCollections("localhost", 27017)
-    val raw = getUser(mongoClient, "user")
+    val raw = getUser(mongoClient, "user_collection")
     val users = raw._1
     val relations = raw._2
     val stock = getStock(mongoClient, "stock")
@@ -19,39 +19,57 @@ object GraphxDemo{
 
     val graph = getGraphx(vertexes, relations)
 
-    val facts: RDD[String] =
-      graph.triplets.map(triplet => triplet.srcAttr + " " +triplet.attr + " " + triplet.dstAttr)
-    facts.collect.foreach(println(_))
+    relations.foreach(println(_))
+
+//    val facts: RDD[String] =
+//      graph.triplets.map(triplet => triplet.srcAttr + " " +triplet.attr + " " + triplet.dstAttr)
+//    graph.triplets.filter(e => e.srcId == "".toLong)
+//    facts.collect.foreach(println(_))
   }
 
-  def getGraphx(vertex: Array[(VertexId, String)], edge: Array[Edge[(String)]]): Graph[String, String] = {
+  def getGraphx(vertex: Array[(VertexId, (String, Long, Long, Int, Long))],
+                edge: Array[Edge[(Long)]]): Graph[(String, Long, Long, Int, Long), Long] =
+  {
     val sc = new SparkContext("local[2]", "graphx")
-    val users: RDD[(VertexId, String)] = sc.parallelize(vertex)
-    var relations: RDD[Edge[String]] = sc.parallelize(edge)
-    val defaultUser = "default"
+    val users: RDD[(VertexId, (String, Long, Long, Int, Long))] = sc.parallelize(vertex)
+    val relations: RDD[Edge[Long]] = sc.parallelize(edge)
+//    val defaultUser = "default"
 
-    Graph(users, relations, defaultUser)
+    Graph(users, relations)
   }
 
   // get user vertex
-  def getUser(mongoClient: MongoClient, collection: String): (Array[(Long, String)], Array[Edge[String]]) = {
+  def getUser(mongoClient: MongoClient, collection: String):
+  (Array[(Long, (String, Long, Long, Int, Long))],
+    Array[Edge[Long]]) = {
     val userCollection = mongoClient("test")(collection)
     val filter_user = MongoDBObject(
       "id" -> 1,
       "stock_list" -> 1,
-      "name" -> 1
+      "name" -> 1,
+      "followers_count" -> 1,
+      "post_count" -> 1,
+      "verified" -> 1,
+      "cube_count" -> 1
     )
 
     val rawData = userCollection.find(MongoDBObject.empty, filter_user).limit(1000).toArray
-    val users = rawData.map(e => (e.get("id").toString.toLong, e.get("name").toString))
-    val relations = rawData.map(e => Tuple2(e.get("id").toString.toLong, e.as[BasicDBList]("stock_list")))
+    val users = rawData.map(e =>
+      (e.get("id").toString.toLong,
+        (e.get("name").toString,
+        e.get("followers_count").toString.toLong,
+        e.get("post_count").toString.toLong,
+        if(e.get("verified").toString == "false") 0 else 1,
+        e.get("cube_count").toString.toLong)))
+
+    val relations = rawData.map(e => (e.get("id").toString.toLong, e.as[BasicDBList]("stock_list")))
       .filter(e => e._2.length != 0)
       .flatMap(toRelation)
     (users, relations)
   }
 
   // get stock vertex
-  def getStock(mongoClient: MongoClient, collection: String): Array[(Long, (String))] = {
+  def getStock(mongoClient: MongoClient, collection: String): Array[(Long, (String, Long, Long, Int, Long))] = {
       val stockCollection = mongoClient("test")(collection)
       val filter_stock = MongoDBObject(
         "_id" -> 0,
@@ -62,12 +80,14 @@ object GraphxDemo{
       stockCollection.find(MongoDBObject.empty, filter_stock).toArray
           .filter(e => !isNumber(e.get("full_code").toString))
         .map(e => Tuple2(e.get("full_code").toString.substring(2).toLong, e.get("name").toString))
+      .map(e => (e._1, (e._2, "0".toLong, "0".toLong, 0, "0".toLong)))
   }
 
-  def toRelation(tuple2: Tuple2[Long, BasicDBList]): Array[Edge[String]] = {
+  def toRelation(tuple2: Tuple2[Long, BasicDBList]): Array[Edge[Long]] = {
     tuple2._2
       .filter(e => isNumber(e.asInstanceOf[BasicDBObject].getString("reduced_code")))
-        .map(e => Edge(tuple2._1, e.asInstanceOf[BasicDBObject].get("reduced_code").toString.toLong, "subscribe")).toArray
+        .map(e => Edge(tuple2._1, e.asInstanceOf[BasicDBObject].get("reduced_code").toString.toLong,
+          e.asInstanceOf[BasicDBObject].get("createAt").toString.toLong)).toArray
   }
 
   def isNumber(id: String): Boolean = {
